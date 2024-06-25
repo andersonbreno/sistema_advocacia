d15ba3d152bf3e3ddcdb11e3c23f7e0cc2b6df21
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2024-06-25 17:36:34 -0300

    Retirada do comentários dos comando para migrações do bd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,6 @@ done
 echo "Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
 #python3 manage.py collectstatic --noinput
-#python3 manage.py makemigrations --noinput
-#python3 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 python3 manage.py runserver 0.0.0.0:8000 --noreload

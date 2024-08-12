4cabe7d07dcc25d0f31909fc678540f44363fea0
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2024-08-12 14:55:56 -0300

    Inclui a View para o app parceiros

--- parceiros/views.py
+++ parceiros/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
-from processos.models import Processo
 from .forms import ParceiroForm
 from .models import Parceiros
-import base64
-from django.core.files.base import ContentFile
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db import transaction
 from django.contrib import messages
 
 class ParceiroListView(LoginRequiredMixin, ListView):
@@ -20,8 +15,27 @@ class ParceiroListView(LoginRequiredMixin, ListView):
 class ParceiroCreatView(LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('logins')
     model = Parceiros
+    form_class = ParceiroForm
     template_name = 'parceiros/parceiros_form.html'
     success_url = reverse_lazy('parceiros:list')
 
-    def form_valid(self, form):
+class ParceiroDetailView(LoginRequiredMixin, DetailView):    
+    login_url = reverse_lazy('logins')
+    model = Parceiros
+    context_object_name = 'parceiro'
+    template_name = 'parceiros/parceiros_detail.html'
+    success_url = reverse_lazy('parceiros:list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        parceiro = self.get_object()
+        context['parceiros_relacionados'] = Processo.objects.filter(parceiro=parceiro)
+        return context
+    
+class ParceirosUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    model = Parceiros
+    form_class = ParceiroForm
+    template_name = 'parceiros/parceiros_form.html'
+    success_url = reverse_lazy('parceiros:list')
 

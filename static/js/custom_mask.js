c8a2d08f01a6dc2ce72b98dcfe9e8e579e190563
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2025-05-05 01:00:05 -0300

    Inclui a máscara do número de processo

--- static/js/custom_mask.js
+++ static/js/custom_mask.js
@@ -4,5 +4,6 @@ $(document).ready(function () {
     $('.mask-telefone').mask('(00) 00000-0000');  // Permite 15 dígitos (9º dígito) 
     $(".cpf-mask").mask("000.000.000-00", { reverse: true });
     $(".mask-cnpj").mask("00.000.000/0000-00", { reverse: true });
-    $(".mask-cep").mask("00000-000");
+    $(".mask-cep").mask("000.00-000");
+    $(".process-mask").mask("0000000-00.0000.0.00.0000");
 });

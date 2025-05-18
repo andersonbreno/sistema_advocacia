b56dd34263fd1267a900269e70bc00053ff90214
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2025-05-18 20:00:08 -0300

    Refatora o custom_mask.py

--- static/js/custom_mask.js
+++ static/js/custom_mask.js
@@ -1,7 +1,7 @@
 // static/js/admin_custom_mask.js
 
 $(document).ready(function () {
-    $('.mask-telefone').mask('(00) 00000-0000');  // Permite 15 dígitos (9º dígito) 
+    $('.mask-telefone').mask("(00) 00000-0000");  // Permite 15 dígitos (9º dígito) 
     $(".cpf-mask").mask("000.000.000-00", { reverse: true });
     $(".mask-cnpj").mask("00.000.000/0000-00", { reverse: true });
     $(".mask-cep").mask("000.00-000");

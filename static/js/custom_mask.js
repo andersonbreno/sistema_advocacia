08a4db26c49e43797a94cb9616010804782e53f6
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2025-05-19 09:38:45 -0300

    Refatora models

--- static/js/custom_mask.js
+++ static/js/custom_mask.js
@@ -5,5 +5,12 @@ $(document).ready(function () {
     $(".cpf-mask").mask("000.000.000-00", { reverse: true });
     $(".mask-cnpj").mask("00.000.000/0000-00", { reverse: true });
     $(".mask-cep").mask("000.00-000");
-    $(".process-mask").mask("0000000-00.0000.0.00.0000");
+    $(".process-mask").mask("0000000-00.0000.0.00.0000", {
+    reverse: false,
+    placeholder: "Digite o número do processo",
+    onComplete: function(value) {
+        // Garante que o valor está no formato correto ao completar
+       //console.log("Processo formatado:", value);
+    }
+    })
 });

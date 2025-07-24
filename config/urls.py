745b520f7be9eb1419362c20208e6e912ec21bbe
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2025-07-24 00:47:28 -0300

    Refatora login e logout

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
+from pages.views import custom_logout
 from config import settings
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
@@ -27,14 +29,20 @@ admin.site.index_title = settings.ADMIN_INDEX_TITLE
 
 
 urlpatterns = [
+    path('login/', 
+         auth_views.LoginView.as_view(
+             template_name='admin/login.html',
+             redirect_authenticated_user=True
+         ), 
+         name='login'),
+    path('logout/', custom_logout, name='logout'),
     path("admin/", admin.site.urls),
     path('', include(('pages.urls', 'pages'), namespace='pages')),  # Inclui as URLs do app pages
     path('cadastro/', include('cadastro.urls')), # Inclui as URLs do app cadastro
-    # path('clientes/', include('clientes.urls')),  # Inclui as URLs do app clientes
+    path('clientes/', include('clientes.urls')),  # Inclui as URLs do app clientes
     path('parceiros/', include('parceiros.urls')), # Inclui as URLs do app parceiros
-    # path('processos/', include('processos.urls')),  # Inclui as URLs do app processos
+    path('processos/', include('processos.urls')),  # Inclui as URLs do app processos
     path('tarefas/', include('tarefas.urls')), # Inclui as URLs do app tarefas
-    path('', include('usuarios.urls')),
     # Redirecionar /favicon.ico para o favicon nos arquivos estáticos
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ]
